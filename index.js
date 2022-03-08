87771c73be6c498c09b4950fe44ccc43bd0309aa
Olatunji Olayinka <olatunji.weber@gmail.com>
2022-03-08 06:38:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,23 +20,20 @@ function checkWinner(player1, player2){
   }
 }
 
-//var choosenImage2 = ".dice" + randomNumber1 + "png";
-// img1.src = choosenImage1;
-//img2.src = choosenImage2;
-// document.querySelector(".img1").src = choosenImage1;
-// document.querySelector(".dice .img2").src = choosenImage2;
-
 dice1.addEventListener('click', function(){
   var play1 = randomDiceRoll();
   document.querySelector(".dice .img1").src = play1[0];
   player1Roll = play1[1];
-  document.querySelector("h1").textContent = checkWinner(player1Roll, player2Roll);
+  if((player1Roll !== 0) && (player2Roll !== 0)){
+    document.querySelector("h1").textContent = checkWinner(player1Roll, player2Roll);
+  }
 })
 
 dice2.addEventListener('click', function(){
   var play2 = randomDiceRoll();
   document.querySelector(".dice .img2").src = play2[0];
   player2Roll = play2[1];
-  // console.log( checkWinner(player1Roll, player2Roll) );
-  document.querySelector("h1").textContent = checkWinner(player1Roll, player2Roll);
+  if((player1Roll !== 0) && (player2Roll !== 0)){
+    document.querySelector("h1").textContent = checkWinner(player1Roll, player2Roll);
+  }
 })
